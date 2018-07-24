5b4925553cf2f743369d74692e6801b0337528cf
sunwei19910119 <754293591@qq.com>
2018-07-24 16:01:55 +0800

    Topic detail page"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'Laravel') - Sun </title>
-
+    <meta name="description" content="@yield('description', 'Laravel')" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('styles')
