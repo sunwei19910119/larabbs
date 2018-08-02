7e756bc91f7c52242ddf9bb7adb6b0fd656a8596
sunwei19910119 <754293591@qq.com>
2018-08-02 09:00:51 +0800

    增加图片验证码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -10,7 +10,19 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captchaData = \Cache::get($request->captcha_key);
+
+        if(!$captchaData){
+            return $this->response->error('图片验证码已失效',422);
+        }
+
+        if(!hash_equals($captchaData['code'],$request->captcha_code)){
+            //验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
 
         if (!app()->environment('production')) {
             $code = '1234';
@@ -23,8 +35,9 @@ class VerificationCodesController extends Controller
                     'content'  =>  "您的验证码是{$code}，请于10分钟内填写。如非本人操作，请忽略本短信。"
                 ]);
             } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-                $message = $exception->getException('qcloud')->getMessage();
-                return $this->response->errorInternal($message ?? '短信发送异常');
+                $response = $exception->getResponse();
+                $result = json_decode($response->getBody()->getContents(), true);
+                return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
             }
         }
 
@@ -32,6 +45,8 @@ class VerificationCodesController extends Controller
         $expiredAt = now()->addMinutes(10);
         // 缓存验证码 10分钟过期。
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
