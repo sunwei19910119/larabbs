3b81613db1d0eb669d53be1015c96b96bb581a4d
sunwei19910119 <754293591@qq.com>
2018-08-07 13:13:24 +0800

    删除回复

--- app/Http/Requests/Api/UserRequest.php
+++ app/Http/Requests/Api/UserRequest.php
@@ -2,19 +2,8 @@
 
 namespace App\Http\Requests\Api;
 
-use Dingo\Api\Http\FormRequest;
-
 class UserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
 
     public function rules()
     {
