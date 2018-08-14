6a143e8b24d7719ef328703953d19ba8d48e2127
sunwei19910119 <754293591@qq.com>
2018-08-14 14:01:54 +0800

    weapp users store

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -65,4 +65,51 @@ class UsersController extends Controller
     {
         return $this->response->collection($user->getActiveUsers(), new UserTransformer());
     }
+
+    public function weappStore(UserRequest $request){
+        //缓存中是否存在对应的key
+        $verifyData = \Cache::get($request->verification_key);
+        if(!$verifyData){
+            return $this->response->error('验证码已失效', 422);
+        }
+
+        if(!hash_equals($verifyData['code'],$request->verification_code)){
+            // 返回401
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        //获取微信的openid和session_key
+        $miniProgram = \EasyWeChat::miniProgram();
+        $data = $miniProgram->auth->session($request->code);
+
+        if (isset($data['errcode'])) {
+            return $this->response->errorUnauthorized('code 不正确');
+        }
+        // 如果 openid 对应的用户已存在，报错403
+        $user = User::where('weapp_openid', $data['openid'])->first();
+        if ($user) {
+            return $this->response->errorForbidden('微信已绑定其他用户，请直接登录');
+        }
+
+        // 创建用户
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => bcrypt($request->password),
+            'weapp_openid' => $data['openid'],
+            'weixin_session_key' => $data['session_key'],
+        ]);
+
+        // 清除验证码缓存
+        \Cache::forget($request->verification_key);
+
+        // meta 中返回 Token 信息
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
+    }
 }
